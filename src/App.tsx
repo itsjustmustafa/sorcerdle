e5eec0cdc7f401ab1faae9f630233050baeb7ec8
= <24886321+itsjustmustafa@users.noreply.github.com>
2024-08-21 19:46:19 +1000

    replaced no-value symbol with emoji

--- src/App.tsx
+++ src/App.tsx
@@ -315,7 +315,7 @@ function App() {
           if(targetValue >= 0){
             emoji = " ⬇️";
           }else{
-            emoji = " 🛇"
+            emoji = " 🚫"
           }
           style = "wrong";
         }
